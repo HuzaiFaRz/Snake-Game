99bd3815fa0f851f87e468f4e039ac09825cb6fc
HuzaiFa A <167712059+HuzaiFaRz@users.noreply.github.com>
2024-06-19 06:11:34 -0700

    Some

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+var snake = document.querySelector(".snake");
+console.log(snake);
+
+// snake.addEventListener("keyup", function (y) {
+//   console.log(y);
+// });
+
 document.body.addEventListener("keyup", function (e) {
+  function mover(u) {
+    snake.style.top = clientY + "px";
+  }
+
   if (e.key === "ArrowUp") {
+    mover();
     console.log("ArrowUp");
   } else if (e.key === "ArrowDown") {
     console.log("ArrowDown");
