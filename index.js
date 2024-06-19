fac6f48507796a0a32d2d4c86a720058cc0f373d
HuzaiFa A <167712059+HuzaiFaRz@users.noreply.github.com>
2024-06-19 05:17:47 -0700

    Some

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-console.log(window.requestAnimationFrame);
-window.addEventListener("keyup", () => {
-console.log("djsjh");
+document.body.addEventListener("keyup", function (e) {
+  if (e.key === "ArrowUp") {
+    console.log("ArrowUp");
+  } else if (e.key === "ArrowDown") {
+    console.log("ArrowDown");
+  } else if (e.key === "ArrowLeft") {
+    console.log("ArrowLeft");
+  } else if (e.key === "ArrowRight") {
+    console.log("ArrowRight");
+  }
 });
