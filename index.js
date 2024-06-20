f0fec4dc465860f1476e05e70e167f6a7ddfa8f9
HuzaiFa A <167712059+HuzaiFaRz@users.noreply.github.com>
2024-06-19 22:46:09 -0700

    Some Added

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
 var snake = document.querySelector(".snake");
-console.log(snake);
+// console.log(snake.clientLeft);
 
 // snake.addEventListener("keyup", function (y) {
 //   console.log(y);
 // });
+// function mover(u) {
+//   // snake.style.top = clientY + "px";
+//   console.log(u);
+// }
 
-document.body.addEventListener("keyup", function (e) {
-  function mover(u) {
-    snake.style.top = clientY + "px";
-  }
-
+// mover();
+var snakeY = snake.clientTop;
+var snakeX = snake.clientLeft;
+document.body.addEventListener("keydown", function (e) {
   if (e.key === "ArrowUp") {
-    mover();
-    console.log("ArrowUp");
+    if (snakeY == 0) {
+      snake.style.top = "0%";
+      console.log("top 0");
+    } else {
+      snake.style.top = snakeY-- + "%";
+    }
   } else if (e.key === "ArrowDown") {
-    console.log("ArrowDown");
+    if (snakeY == 95) {
+      snake.style.top = "95%";
+      console.log("top 95");
+    } else {
+      snake.style.top = snakeY++ + "%";
+    }
   } else if (e.key === "ArrowLeft") {
-    console.log("ArrowLeft");
+    if (snakeX == 0) {
+      snake.style.left = "0%";
+      console.log("left 95");
+    } else {
+      snake.style.left = snakeX-- + "%";
+    }
   } else if (e.key === "ArrowRight") {
-    console.log("ArrowRight");
+    if (snakeX == 98) {
+      snake.style.left = "98%";
+      console.log("right 95");
+    } else {
+      snake.style.left = snakeX++ + "%";
+    }
   }
 });
