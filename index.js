9dabcc44becdabf40a3c26b62aeb402977aeaf01
HuzaiFa A <167712059+HuzaiFaRz@users.noreply.github.com>
2024-06-22 05:02:36 -0700

    Some

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-var snake = document.querySelector(".snake");
+var gameRunner = new Audio("game_runnner.mp3");
+var gamePlay = new Audio("game_play.mp3");
+var gameOver = new Audio("game_over.wav");
+var gameFoodEating = new Audio("food_eating_sound.wav");
+
+gameRunner.play();
+
+// var snake = document.querySelector(".snake");
 // console.log(snake.clientLeft);
 
 // snake.addEventListener("keyup", function (y) {
@@ -10,36 +17,36 @@ var snake = document.querySelector(".snake");
 // }
 
 // mover();
-var snakeY = snake.clientTop;
-var snakeX = snake.clientLeft;
-document.body.addEventListener("keydown", function (e) {
-  if (e.key === "ArrowUp") {
-    if (snakeY == 0) {
-      snake.style.top = "0%";
-      console.log("top 0");
-    } else {
-      snake.style.top = snakeY-- + "%";
-    }
-  } else if (e.key === "ArrowDown") {
-    if (snakeY == 95) {
-      snake.style.top = "95%";
-      console.log("top 95");
-    } else {
-      snake.style.top = snakeY++ + "%";
-    }
-  } else if (e.key === "ArrowLeft") {
-    if (snakeX == 0) {
-      snake.style.left = "0%";
-      console.log("left 95");
-    } else {
-      snake.style.left = snakeX-- + "%";
-    }
-  } else if (e.key === "ArrowRight") {
-    if (snakeX == 98) {
-      snake.style.left = "98%";
-      console.log("right 95");
-    } else {
-      snake.style.left = snakeX++ + "%";
-    }
-  }
-});
+// var snakeY = snake.clientTop;
+// var snakeX = snake.clientLeft;
+// document.body.addEventListener("keydown", function (e) {
+//   if (e.key === "ArrowUp") {
+//     if (snakeY == 0) {
+//       snake.style.top = "0%";
+//       console.log("top 0");
+//     } else {
+//       snake.style.top = snakeY-- + "%";
+//     }
+//   } else if (e.key === "ArrowDown") {
+//     if (snakeY == 95) {
+//       snake.style.top = "95%";
+//       console.log("top 95");
+//     } else {
+//       snake.style.top = snakeY++ + "%";
+//     }
+//   } else if (e.key === "ArrowLeft") {
+//     if (snakeX == 0) {
+//       snake.style.left = "0%";
+//       console.log("left 95");
+//     } else {
+//       snake.style.left = snakeX-- + "%";
+//     }
+//   } else if (e.key === "ArrowRight") {
+//     if (snakeX == 98) {
+//       snake.style.left = "98%";
+//       console.log("right 95");
+//     } else {
+//       snake.style.left = snakeX++ + "%";
+//     }
+//   }
+// });
